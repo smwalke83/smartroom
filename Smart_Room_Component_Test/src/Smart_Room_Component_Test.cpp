d56fa6e12cd2fa6aee9e8db8dc2075a656958ba7
unknown <smwalk83@gmail.com>
2024-01-04 13:48:53 -0700

    Forgot to push before holidays

---
+++ Smart_Room_Component_Test/src/Smart_Room_Component_Test.cpp
@@ -0,0 +1,27 @@
+/* 
+ * Project Test Motion and Rain Sensor
+ * Author: Scott Walker
+ * Date: 12/15/2023
+
+ */
+
+#include "Particle.h"
+const int TESTPIN = D8;
+bool test;
+
+SYSTEM_MODE(SEMI_AUTOMATIC);
+
+void setup() {
+
+pinMode(TESTPIN, INPUT);
+Serial.begin(9600);
+waitFor(Serial.isConnected, 10000);
+
+}
+
+void loop() {
+
+test = digitalRead(TESTPIN);
+Serial.printf("The motion is %i\n", test);
+
+}
