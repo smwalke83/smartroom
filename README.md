f01d0eed09c8bef5f5e81bead5baa3963f76d5ed
unknown <smwalk83@gmail.com>
2024-01-18 16:24:28 -0700

    Final touches

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
+This contains all the documentation from my "smart porch" smartroom project. This was the midterm project for CNM Ingenuity's IoT Deep Dive Boot Camp. 
+
+Coding folders:
+
+    OLED_Display_Test - Primarily testing bitmapping.
+
+    Photodiode_Test
+
+    Servo_Test_Smart_Porch - Servos used to operate windshield wipers.
+
+    Smart_Room_Component_Test - Test multiple digital components.
+
+    Smart_Porch_Final_Code - The final code for the project.
+
+Other documentation folders:
+
+    3D Design and Print - All solidworks and .stl files for the 3D printed components.
+
+    Bitmapping for OLED - Images (designed in MS Paint and resized online) for bitmapping on the OLED display.
+
+    Drawings, Designs, Planning - PDFs of hand drawn/written plans, schematics, drawings. I typed up some of the initial planning and put it in a Google doc a well (included).
+
+    Fritzing - Includes the fritzing file, schematic JPG, and breadboard JPG.
+
+    Laser Cut and Engrave - File used to cut/engrave the acryllic windshield.
+
+    Pictures - Project photos.
+
 
--- Smart_Porch_Final_Code/src/Smart_Porch_Final_Code.cpp
+++ Smart_Porch_Final_Code/src/Smart_Porch_Final_Code.cpp
@@ -4,7 +4,7 @@
  * Date: 
  */
 
-#include "Particle.h"
+#include "Particle.h"             // header files
 #include "Adafruit_BME280.h"
 #include "Adafruit_GFX.h"
 #include "Adafruit_SSD1306.h"
@@ -28,8 +28,8 @@ const int OLED_RESET = -1;        // lines 27-30 are constants for the BME and O
 const int OLEDADDRESS = 0x3C;
 const int BMEADDRESS = 0x76;
 const int DEGREE = 0xF8;
-const int WEMO1 = 1;              // wemo constants
-const int WEMO2 = 1;
+const int WEMO1 = 1;              // lines 31-41 are wemo and hue constants
+const int WEMO2 = 0;
 const int BULB1 = 1;
 const int BULB2 = 2;
 const int BULB3 = 3;
@@ -37,23 +37,23 @@ const int BULB4 = 4;
 const int BULB5 = 5;
 const int BULB6 = 6;
 const int hueColor = 0;
+const int hueBrightness = 150;
+const int saturation = 0;
 int photoValues[30];              // array that fills with data from the photodiode, to be averaged out for more consistant data
 const int neoColors[7] = {blue, navy, white, pink, purple, maroon, red};  // colors for neopixels, based on temperature
-int neoColorTemp;                 // lines 35-38 are variables to help determine brightness and color of neopixels/hue lights
+int neoColorTemp;                 // lines 44-47 are variables to help determine brightness and color of neopixels/hue lights
 int neoBrightness;
-const int hueBrightness = 150;
 int photoSum;
 int photoAVG;
-int currentTime;                  // lines 39-40 are variables used for a simple timer
+int currentTime;                  // lines 48-49 are variables used for a simple timer
 int lastSecond;
 int n; int x; int y; int z;       // variables used for 'for' loops and iteration
-const int saturation = 0;
-float tempC;                      // lines 42-46 are variables for the BME readings and OLED display
+float tempC;                      // lines 51-55 are variables for the BME readings and OLED display
 float tempF;
 float pressPA;
 float pressHG;
 float humidRH;
-bool onOff;                       // these bool states affect what mode the controller is in - hot/cold, rain/dry, on/off
+bool onOff;                       // these bool states affect what mode the controller is in - hot/cold, rain/dry, on/off, manual/auto
 bool rain;
 bool hot;
 bool wemo1On;
@@ -62,7 +62,7 @@ bool hueisOn;
 bool manual;
 bool motion;
 const int manualModes[6] = {0, 1, 2, 3, 4, 5}; 
-Adafruit_BME280 bme;
+Adafruit_BME280 bme;                                  // objects
 Adafruit_SSD1306 display(OLED_RESET);
 Adafruit_NeoPixel pixel(PIXELCOUNT, SPI1, WS2812B);
 Servo myServo;
@@ -79,30 +79,29 @@ SYSTEM_MODE(MANUAL);
 
 void setup() {
 
-pixel.begin();
+pixel.begin();                                          // pixel setup
 pixel.show();
-display.begin(SSD1306_SWITCHCAPVCC, OLEDADDRESS);
+display.begin(SSD1306_SWITCHCAPVCC, OLEDADDRESS);       // OLED setup
 display.clearDisplay();
 display.display();
-bme.begin(BMEADDRESS);
-WiFi.on();
+bme.begin(BMEADDRESS);                                  // BME280 setup
+WiFi.on();                                              // connecting to network for hue and wemo
 WiFi.clearCredentials();
 WiFi.setCredentials("IoTNetwork");
 WiFi.connect();
-pinMode(RAINSENSOR, INPUT);
+pinMode(RAINSENSOR, INPUT);                             // pinmodes for relevant components
 pinMode(PHOTOSENSOR, INPUT);
 pinMode(MOTIONPIN, INPUT);
-myServo.attach(SERVOPIN);
-onOff = false;
+myServo.attach(SERVOPIN);                               // servo setup
 myServo.write(ANGLE1);
-x = 0;
+x = 0;                                                  // setting bools and variables
 z = 0;
 lastSecond = 0;
+onOff = false;
 wemo1On = false;
 wemo2On = false;
-hueisOn = false;
+hueisOn = true;
 manual = false;
-setHue(BULB2, LOW, hueColor, hueBrightness, saturation);
 
 }
 
@@ -114,7 +113,7 @@ pressPA = bme.readPressure();
 humidRH = bme.readHumidity();
 tempF = (9.0/5.0)*tempC + 32;
 pressHG = pressPA/3386.0;
-neoColorTemp = map(tempF, 60.0, 80.0, 0.0, 6.0);
+neoColorTemp = map(tempF, 65.0, 85.0, 0.0, 6.0);
 photoValues[x%30] = analogRead(PHOTOSENSOR);
 x++;
 photoSum = 0;
@@ -133,22 +132,32 @@ if(!manual){
     pixel.setPixelColor(n, neoColors[neoColorTemp]);
   }
   pixel.show();
-  if(hueisOn == false && photoAVG < 25){
-    setHue(BULB2, HIGH, hueColor, hueBrightness, saturation);
+  if(hueisOn == false && photoAVG < 30){
+    //setHue(BULB1, HIGH, hueColor, hueBrightness, saturation);
+    //setHue(BULB2, HIGH, hueColor, hueBrightness, saturation);
+    //setHue(BULB3, HIGH, hueColor, hueBrightness, saturation);
+    //setHue(BULB4, HIGH, hueColor, hueBrightness, saturation);
+    //setHue(BULB5, HIGH, hueColor, hueBrightness, saturation);
+    setHue(BULB6, HIGH, hueColor, hueBrightness, saturation);
     hueisOn = true;
   }
-  if(hueisOn == true && photoAVG >= 25){
-    setHue(BULB2, LOW, hueColor, hueBrightness, saturation);
+  if(hueisOn == true && photoAVG >= 30){
+    //setHue(BULB1, LOW, hueColor, hueBrightness, saturation);
+    //setHue(BULB2, LOW, hueColor, hueBrightness, saturation);
+    //setHue(BULB3, LOW, hueColor, hueBrightness, saturation);
+    //setHue(BULB4, LOW, hueColor, hueBrightness, saturation);
+    //setHue(BULB5, LOW, hueColor, hueBrightness, saturation);
+    setHue(BULB6, LOW, hueColor, hueBrightness, saturation);
     hueisOn = false;
   }
   if(digitalRead(RAINSENSOR)){
     rain = true;
     rainTimer.startTimer(10000);
   }
-  if(tempF > 70.0){
+  if(tempF > 75.0){
     hot = true;
   }
-  if(tempF <= 70.0){
+  if(tempF <= 75.0){
     hot = false;
   }
   if(rain){
@@ -252,7 +261,12 @@ if(manual){
     pixel.show();
     myServo.write(ANGLE1);
     if(hueisOn == true){
-      setHue(BULB2, LOW, hueColor, hueBrightness, saturation);
+      //setHue(BULB1, LOW, hueColor, hueBrightness, saturation);
+      //setHue(BULB2, LOW, hueColor, hueBrightness, saturation);
+      //setHue(BULB3, LOW, hueColor, hueBrightness, saturation);
+      //setHue(BULB4, LOW, hueColor, hueBrightness, saturation);
+      //setHue(BULB5, LOW, hueColor, hueBrightness, saturation);
+      setHue(BULB6, LOW, hueColor, hueBrightness, saturation);
       hueisOn = false;
     }
   }
@@ -278,7 +292,12 @@ if(manual){
     }
     pixel.show();
     if(hueisOn == false){
-      setHue(BULB2, HIGH, hueColor, hueBrightness, saturation);
+      //setHue(BULB1, HIGH, hueColor, hueBrightness, saturation);
+      //setHue(BULB2, HIGH, hueColor, hueBrightness, saturation);
+      //setHue(BULB3, HIGH, hueColor, hueBrightness, saturation);
+      //setHue(BULB4, HIGH, hueColor, hueBrightness, saturation);
+      //setHue(BULB5, HIGH, hueColor, hueBrightness, saturation);
+      setHue(BULB6, HIGH, hueColor, hueBrightness, saturation);
       hueisOn = true;
     }
     if(!wemo1On){
@@ -304,7 +323,12 @@ if(manual){
     }
     pixel.show();
     if(hueisOn == false){
-      setHue(BULB2, HIGH, hueColor, hueBrightness, saturation);
+      //setHue(BULB1, HIGH, hueColor, hueBrightness, saturation);
+      //setHue(BULB2, HIGH, hueColor, hueBrightness, saturation);
+      //setHue(BULB3, HIGH, hueColor, hueBrightness, saturation);
+      //setHue(BULB4, HIGH, hueColor, hueBrightness, saturation);
+      //setHue(BULB5, HIGH, hueColor, hueBrightness, saturation);
+      setHue(BULB6, HIGH, hueColor, hueBrightness, saturation);
       hueisOn = true;
     }
     if(wemo1On){
@@ -340,7 +364,12 @@ if(manual){
     }
     pixel.show();
     if(hueisOn == false){
-      setHue(BULB2, HIGH, hueColor, hueBrightness, saturation);
+      //setHue(BULB1, HIGH, hueColor, hueBrightness, saturation);
+      //setHue(BULB2, HIGH, hueColor, hueBrightness, saturation);
+      //setHue(BULB3, HIGH, hueColor, hueBrightness, saturation);
+      //setHue(BULB4, HIGH, hueColor, hueBrightness, saturation);
+      //setHue(BULB5, HIGH, hueColor, hueBrightness, saturation);
+      setHue(BULB6, HIGH, hueColor, hueBrightness, saturation);
       hueisOn = true;
     }
     if(!wemo1On){
@@ -376,7 +405,12 @@ if(manual){
     }
     pixel.show();
     if(hueisOn == false){
-      setHue(BULB2, HIGH, hueColor, hueBrightness, saturation);
+      //setHue(BULB1, HIGH, hueColor, hueBrightness, saturation);
+      //setHue(BULB2, HIGH, hueColor, hueBrightness, saturation);
+      //setHue(BULB3, HIGH, hueColor, hueBrightness, saturation);
+      //setHue(BULB4, HIGH, hueColor, hueBrightness, saturation);
+      //setHue(BULB5, HIGH, hueColor, hueBrightness, saturation);
+      setHue(BULB6, HIGH, hueColor, hueBrightness, saturation);
       hueisOn = true;
     }
     if(wemo1On){
@@ -396,8 +430,21 @@ if(manual){
       pixel.clear();
       pixel.show();
       myServo.write(ANGLE1);
+      if(wemo1On){
+        wemo1.wemoWrite(LOW);
+        wemo1On = false;
+      }
+      if(wemo2On){
+        wemo2.wemoWrite(LOW);
+        wemo2On = false;
+      }
       if(hueisOn == true){
-        setHue(BULB2, LOW, hueColor, hueBrightness, saturation);
+        //setHue(BULB1, LOW, hueColor, hueBrightness, saturation);
+        //setHue(BULB2, LOW, hueColor, hueBrightness, saturation);
+        //setHue(BULB3, LOW, hueColor, hueBrightness, saturation);
+        //setHue(BULB4, LOW, hueColor, hueBrightness, saturation);
+        //setHue(BULB5, LOW, hueColor, hueBrightness, saturation);
+        setHue(BULB6, LOW, hueColor, hueBrightness, saturation);
         hueisOn = false;
       }
     }
@@ -408,10 +455,24 @@ if(manual){
         pixel.setPixelColor(n, neoColors[neoColorTemp]);
       }
       pixel.show();
+      if(wemo1On){
+        wemo1.wemoWrite(LOW);
+        wemo1On = false;
+      }
+      if(wemo2On){
+        wemo2.wemoWrite(LOW);
+        wemo2On = false;
+      }
       if(hueisOn == false){
-        setHue(BULB2, HIGH, hueColor, hueBrightness, saturation);
+        //setHue(BULB1, HIGH, hueColor, hueBrightness, saturation);
+        //setHue(BULB2, HIGH, hueColor, hueBrightness, saturation);
+        //setHue(BULB3, HIGH, hueColor, hueBrightness, saturation);
+        //setHue(BULB4, HIGH, hueColor, hueBrightness, saturation);
+        //setHue(BULB5, HIGH, hueColor, hueBrightness, saturation);
+        setHue(BULB6, HIGH, hueColor, hueBrightness, saturation);
         hueisOn = true;
       }
+      myServo.write(ANGLE1);
     }
   }
 }
